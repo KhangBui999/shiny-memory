c251a5367e29ef4ba03974a7849169f10a725f4f
khang <khang@khangs-mbp>
2019-10-31 13:27:13 +1100

    [Feature] Database utility file has been added

---
+++ src/clarify/Model/Task.java
@@ -0,0 +1,12 @@
+package clarify.Model;
+
+/*
+* TODO:
+* Just make this a normal object class. Haha. I'll add any logical thing we
+* need from this class later on.
+*/
+
+
+public class Task {
+    
+}
