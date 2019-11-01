a9b0ed1fab86ff7b0a2d11fa738bd46dfcf83b05
BRIAN <BRIAN@DESKTOP-VOUQR1N>
2019-11-01 16:07:47 +1100

    [task class, similar to music]

--- src/clarify/Model/Task.java
+++ src/clarify/Model/Task.java
@@ -1,12 +1,76 @@
 package clarify.Model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /*
 * TODO:
 * Just make this a normal object class. Haha. I'll add any logical thing we
 * need from this class later on.
 */
 
-//hello world 
 public class Task {
+    
+    private StringProperty title;
+    private StringProperty desc;
+    private StringProperty doDate;
+    private StringProperty dueDate;
+    private StringProperty priority;
+    
+     public Task() {
+        this("","","","","");
+    }
+
+    public Task(String title, String desc, String doDate, String dueDate, String priority) {
+        this.title = new SimpleStringProperty(title);
+        this.desc= new SimpleStringProperty(desc);
+        this.doDate= new SimpleStringProperty(doDate);
+        this.dueDate= new SimpleStringProperty(dueDate);
+        this.priority = new SimpleStringProperty(priority);
+
+    }
+    public final StringProperty getTitleProperty() {
+        return title;
+    }
+
+    public final void setTitle(StringProperty title) {
+        this.title = title;
+    }
+
+    public final StringProperty getDescriptionProperty() {
+        return desc;
+    }
+
+    public final void setDescription(StringProperty desc) {
+        this.desc = desc;
+    }
+
+    public final StringProperty getDoDateProperty() {
+        return doDate;
+    }
+
+    public final void setDoDate(StringProperty doDate) {
+        this.doDate = doDate;
+    }
+
+    public final StringProperty getDueDateProperty() {
+        return dueDate;
+    }
+
+    public final void setDueDate(StringProperty dueDate) {
+        this.dueDate = dueDate;
+    }
+    public final StringProperty getPriorityProperty() {
+        return priority;
+    }
+
+    public final void setPriority(StringProperty priority) {
+        this.priority = priority;
+    }
+    
+    
+
+
+
     
 }
