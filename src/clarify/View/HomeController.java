450ffeed8621dea7d8e6588b266ed3fa510dd331
shara <56857006+SgilG91@users.noreply.github.com>
2019-11-02 13:40:46 +1100

    [feature] pie chart works!

--- src/clarify/View/HomeController.java
+++ src/clarify/View/HomeController.java
@@ -1,4 +1,3 @@
- 
 package clarify.View;
 
 import clarify.Util.Database;
@@ -17,6 +16,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
 import javafx.scene.chart.PieChart;
 
 /**
@@ -37,7 +37,7 @@ public class HomeController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
 
         try {
-            // TODO
+
             loadPieChart();
         } catch (SQLException ex) {
             Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
@@ -48,7 +48,7 @@ public class HomeController implements Initializable {
     public void loadPieChart() throws SQLException {
         System.out.println("Loading Pie Chart of My Life");
 
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<String> entriesList = new ArrayList<>();
 
         Connection conn = DriverManager.getConnection("jdbc:sqlite:INFS2605.db");
         Statement st = conn.createStatement();
@@ -57,16 +57,30 @@ public class HomeController implements Initializable {
         ResultSet rs = st.executeQuery(selectQuery);
 
         while (rs.next()) {
-            tasks.add(rs.getString(1));
+            entriesList.add(rs.getString(1));
         }
 
-        System.out.println(tasks);
+        System.out.println("Activities entered: "+ entriesList);
+
+        ArrayList<String> durations = new ArrayList<>();
+
+        String selectQuerys = "SELECT ((strftime('%s',endTime) - strftime('%s',startTime))/60)FROM ENTRIES WHERE ent_id IS NOT NULL;";
+        ResultSet rss = st.executeQuery(selectQuerys);
+        while (rss.next()) {
+            durations.add(rss.getString(1));
+        }
+
+        //System.out.println(durations);
+        ArrayList<Integer> durationsInt = new ArrayList<Integer>(durations.size());
+        for (String myInt : durations) {
+            durationsInt.add(Integer.valueOf(myInt));
+        }
+        System.out.println("Time spent on each activity (minutes): " + durationsInt);
 
         ObservableList<PieChart.Data> pieChartData
                 = FXCollections.observableArrayList(
-                        //  new PieChart.Data("ta", 13),
-                        new PieChart.Data(tasks.get(0), 25),
-                        new PieChart.Data(tasks.get(1), 10));
+                        new PieChart.Data(entriesList.get(0), durationsInt.get(0)),
+                        new PieChart.Data(entriesList.get(1), durationsInt.get(1)));
 
         lifePieChart.setData(pieChartData);
         lifePieChart.setStartAngle(90);
