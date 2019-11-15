ef35d242c5023caebc5c1e46698ccc189a06bdcf
khang <khang@khangs-mbp>
2019-11-15 23:53:36 +1100

    [Feature] Daily Learnings Data Capture Completed

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,7 @@ public class Main extends Application {
         d.createCategoriesTable();
         d.createEntriesTable();
         d.createTaskTable();
+        d.createReflectionTable();
     }
 
     /**
